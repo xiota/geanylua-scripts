32152b2f0d898a016709b6849f39d570468ecf73
xiota <github@mentalfossa.com>
2021-09-22 08:56:59 -0700

    toggle-menu/events/init.lua - add instructions to start with menu hidden

--- toggle-menu/events/init.lua
+++ toggle-menu/events/init.lua
@@ -1,5 +1,9 @@
--- load and run script to hide menubar on start
+-- load and initialize toggle_menu script
 local lua_path=geany.dirname(geany.script)..geany.dirsep..".."..geany.dirsep
 
 toggle_menu=loadfile(lua_path.."toggle-menu.lua")
 toggle_menu()
+
+-- to start with the menu hidden, run toggle_menu() twice
+-- by uncommenting the following line:
+-- toggle_menu()
