7f229ccbb27a3ef0660dd717ef73d10653420ff5
xiota <aur@mentalfossa.com>
2023-03-08 11:42:28 -0800

    switch-views: update events/init.lua

--- switch-views/events/init.lua
+++ switch-views/events/init.lua
@@ -7,13 +7,16 @@ local lua_path = geany.appinfo().scriptdir..geany.dirsep
     hide   - startup with editor hidden
     *show    - startup with editor visible
     toggle  - startup with the editor toggled from the last editor state
+
+  Note: Options other than "show" may not work.
 --]]
 
 editor_start = "show"
 sidebar_start = "show"
 
-toggle_editor = loadfile(lua_path.."toggle-editor.lua")
-toggle_editor()
+toggle_editor = loadfile(lua_path.."switch-views"..geany.dirsep.."toggle-editor.lua")
 
-toggle_sidebar = loadfile(lua_path.."toggle-sidebar.lua")
+toggle_sidebar = loadfile(lua_path.."switch-views"..geany.dirsep.."toggle-sidebar.lua")
+
+toggle_editor()
 toggle_sidebar()
