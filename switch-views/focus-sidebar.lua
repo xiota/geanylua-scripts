336461fd50b4a8b5e5b85ebb8989e737a2ac51af
xiota <aur@mentalfossa.com>
2023-03-08 13:11:13 -0800

    switch-views: - add focus-editor and focus-sidebar scripts - fix typo

--- switch-views/focus-sidebar.lua
+++ switch-views/focus-sidebar.lua
@@ -2,8 +2,4 @@ lua_path=geany.appinfo().scriptdir..geany.dirsep
 dofile(lua_path.."toggle-functions.lua")
 -- ---------------
 
-editor_show()
-sidebar_show()
-update()
-
-editor_focus()
+focus_sidebar()
